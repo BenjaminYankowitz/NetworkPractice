fde5af718276b48b5166f7cfcfb2548f649f2fec
BenjaminYankowitz <Benjamin.Yankowitz@gmail.com>
2026-06-16 14:16:12 -0400

    removed old C++ location

--- orderManage/kafka/kafkaTopics.h
+++
@@ -1,9 +0,0 @@
-#ifndef KAFKA_TOPICS_H
-#define KAFKA_TOPICS_H
-#include <kafka/Types.h>
-
-namespace KafkaTopic {
-  static const kafka::Topic Audit = "audit";
-} // namespace KafkaTopic
-
-#endif // KAFKA_TOPICS_H
